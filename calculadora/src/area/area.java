c114496d47502cb8aedaad1e389e5d84e2a7245c
BASS304 <pazasebas@gmail.com>
2025-03-02 22:14:47 -0500

    Add files via upload

---
+++ calculadora/src/area/area.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package area;
+
+import calculadora.calculadora;
+
+/**
+ *
+ * @author emjuc
+ */
+public class area {
+
+    public static void main(String[] args) {
+        
+        calculadora calcu = new calculadora();
+        calcu.setVisible(true);
+        calcu.setLocationRelativeTo(null);
+        
+    }
+    
+}
